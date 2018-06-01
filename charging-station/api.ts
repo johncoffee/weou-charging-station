4b386b409cbd4ca9d025ebb6803bc1590759db68
johncoffee <github2017@ragelse.dk>
2018-06-01 12:43:29 +0200

    disconnect WIP

--- charging-station/api.ts
+++ charging-station/api.ts
@@ -1,7 +1,7 @@
 import * as Koa from 'koa'
 import { Context } from 'koa'
 import { CableState, ChargingState, ChargingStation, wait } from './hardware.js'
-import { getBalanceOf, transferFrom } from './eth.js'
+import { getBalanceOf } from './eth.js'
 import { httpRequest } from './http-request.js'
 import { URL } from 'url'
 import { getCo2, getPrice } from './market.js'
@@ -45,16 +45,29 @@ app.listen((process.env.PORT || 3000),  async() => {
 
 
 async function updateHandler () {
-  const station = new ChargingStation('0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca', 'http://10.170.111.0:8088')
-  // const station = new ChargingStation('0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca', 'http://localhost:8888')
+  // const station = new ChargingStation('0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca', 'http://10.170.111.0:8088')
+  const station = new ChargingStation('0x51f8a5d539582eb9bf2f71f66bcc0e6b37abb7ca', 'http://localhost:8888')
+  let state:ChargingState = ChargingStation.handle.get(station.id)
+  let newState:ChargingState
   try {
-    await station.pollStatus()
+    newState = await station.pollStatus()
   }
   catch (e) {
     console.error(e)
   }
+
+  if (newState.cable !== state.cable) {
+    if (newState.cable === CableState.NO_CABLE) {
+      onDisconnect(station.id)
+    }
+  }
+
   await wait(5)
   updateHandler()
+}
+
+function onDisconnect(id:string) {
+  console.debug("Disconnect")
 }
 
 updateHandler()
@@ -70,16 +83,18 @@ routes.set('/start', async (ctx:Context) => {
   const station = new ChargingStation(chargingStagingAddress, baseUrl.toString())
   const state = ChargingStation.handle.get(station.id)
 
-  if (state.cable !== CableState.NO_CABLE && budget > 0) {
+  const isCharging = (state.kW > 0.1 && state.cable === CableState.READY_TO_CHARGE || state.cable === CableState.READY_TO_NEGOTIATE)
+  const canStart = (budget > 10 && !isCharging)
+
+  if (canStart) {
     await station.setCharging(true)
-    console.log("STARTED charging!")
-    const returnFunds:number = await station.chargeBudget(200, budget) // dont await this, it will run for hours
-    if (returnFunds > 0) {
-      console.log(`transfer rest to ${returnFundsAddress} ${returnFunds}`)
-      transferFrom(chargingStagingAddress, returnFundsAddress, returnFunds)
-        .catch(err => console.error(err))
-    }
     ctx.response.status = 204
+    // const start_kWh = state.kWhTotal
+    // const returnFunds:number = await station.chargeBudget(200, budget) // dont await this, it will run for hours
+    // if (returnFunds > 0) {
+    //   console.log(`transfer rest to ${returnFundsAddress} ${returnFunds}`)
+    //   transferFrom(chargingStagingAddress, returnFundsAddress, returnFunds)
+    //     .catch(err => console.error(err))
   }
   else {
     ctx.response.body = {error: {message: "Not connected"}}
