01b30d6263b15a3aee339982c4217bdabf95adce
johncoffee <github2017@ragelse.dk>
2018-06-05 16:59:29 +0200

    checksum addr

--- charging-station/config.ts
+++ charging-station/config.ts
@@ -1,3 +1,3 @@
-export const privateKey = `0x` + `6724b72fdbf512ff825787602b761d2f9983c7e828ea905284f43ea9a19faa01` // 0xbd65990e92e07567c472e4a6a24291bf6aefcdb9
+export const privateKey = `0x` + `6724b72fdbf512ff825787602b761d2f9983c7e828ea905284f43ea9a19faa01` // 0xBd65990E92e07567c472e4A6a24291Bf6AefCdb9
 export const networkId = 1337
 export const contractAddress = '0x6C041FB1E17Aa0e95af5b078C45c7397fe3cAA0b'
